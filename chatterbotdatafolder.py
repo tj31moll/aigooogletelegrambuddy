1db60f5c17367bc7b3b09b3faba7e46d5a7af367
tj31moll <91799649+tj31moll@users.noreply.github.com>
2023-03-05 21:26:02 -0500

    Update chatterbotdatafolder.py

--- chatterbotdatafolder.py
+++ chatterbotdatafolder.py
@@ -1,11 +1,11 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
-from chatterbot.storage import FileStorageAdapter
+from chatterbot.storage import JsonFileStorageAdapter
 
 chatbot = ChatBot(
     'MyChatBot',
-    storage_adapter='chatterbot.storage.FileStorageAdapter',
-    database_uri='/app/data/conversations.json',
+    storage_adapter='chatterbot.storage.JsonFileStorageAdapter',
+    database='./data/conversations.json',
     output_format='json'
 )
 
